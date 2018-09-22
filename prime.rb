2021de55fbac8a553f911219bc34aa71f72065e3
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-22 20:49:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number_argument)
+  return false if number_argument < 2 
+  (2..number_argument -1).each do |num|
+    if (number_argument % num) == 0 
+      return false 
+  end 
+end 
+true 
+end 

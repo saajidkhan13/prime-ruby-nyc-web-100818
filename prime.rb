85c6d8a6c0743573e2ee01da735ddac3c84427a1
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-22 20:43:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,5 +7,25 @@ def prime?(number)
     newArray << i
     i += 1 
   end
-end 
+
+newArray.each do |value|
+  if primeNumbers.include?(value) == false 
+    primeNumbers << value 
+    excludeValues(value, newArray) 
+  end 
+  
+  if primeNumbers.include?(number)
+    return true 
+  else 
+    return false 
+  end 
+  
+  def excludevalues(int, newArray)
+    newArray.each do |number| 
+      if number%int == 0 
+        newArray.delete(number) 
+      end 
+    end 
+    
+  end  
     
